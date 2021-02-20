a54f332f8ecfcb4199661ed6018594b1542f502b
Samuel Wu <swu11027@gmail.com>
2021-02-20 13:09:22 -0800

    implemented the optional section and the Readme.md

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -10,16 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var card: UIView!
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var backLabel: UILabel!
+    
+    @IBOutlet weak var btnOptionOne: UIButton!
+    @IBOutlet weak var btnOptionTwo: UIButton!
+    @IBOutlet weak var btnOptionThree: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        card.layer.cornerRadius = 20.0
+        card.layer.shadowRadius = 15.0
+        card.layer.shadowOpacity = 0.2
+        
+        frontLabel.layer.cornerRadius = 20.0
+        backLabel.layer.cornerRadius = 20.0
+        
+        frontLabel.clipsToBounds = true
+        backLabel.clipsToBounds = true
+        
+        btnOptionOne.layer.cornerRadius = 20.0
+        btnOptionTwo.layer.cornerRadius = 20.0
+        btnOptionThree.layer.cornerRadius = 20.0
+        
+        btnOptionOne.layer.borderWidth = 3.0
+        btnOptionOne.layer.borderColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        btnOptionTwo.layer.borderWidth = 3.0
+        btnOptionTwo.layer.borderColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        btnOptionThree.layer.borderWidth = 3.0
+        btnOptionThree.layer.borderColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        
     }
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
-        frontLabel.isHidden = true
+        frontLabel.isHidden = !frontLabel.isHidden
+    }
+    @IBAction func didTapOptionOne(_ sender: Any) {
+        btnOptionOne.isHidden = true
     }
     
+    @IBAction func didTapOptionTwo(_ sender: Any) {
+        btnOptionTwo.backgroundColor = #colorLiteral(red: 0.3839578032, green: 1, blue: 0.4577614665, alpha: 1)
+        frontLabel.isHidden = true
+        btnOptionThree.isHidden = true
+        btnOptionOne.isHidden = true
+    }
+    
+    @IBAction func didTapOptionThree(_ sender: Any) {
+        btnOptionThree.isHidden = true
+    }
 }
 
---
+++ Readme.md
@@ -0,0 +1,24 @@
+## Flashcards
+
+📝 `NOTE:` Paste the README templates for each subsequent lab here at the top, (i.e. lab 2, 3, 4). This will show a history of your development process including which users stories you completed and how your app looked and functioned at each step.
+
+## Lab 1
+
+### App Description
+This app is a flashcard app that contains a question on the screen of the the app with three answer choices below. The user of the app has two options: click on the answer choices below or directly reveal the answer to the question. If the answer choice tapped on is wrong, the answer choice will disappear. If the answer choice tapped on is the correct answer, the question box will be replace with the answer box and the answer choice will have a green background, removing all of the answer choices except for the correct one. To directly access the answer, the user is able to tap on the question box, which will cause the question box to be replace with the answer box. To go back to the question box, the user can simply tap on the answer box.
+
+### App Walk-though
+
+<img src="http://g.recordit.co/7auNX35W83.gif" width=200><br>
+
+
+## Required
+- [x] Create New Project in Xcode
+- [x] Add a view for the front side of the flashcard to display the question
+- [x] Add a view for the back side of the flashcard to display the answer
+- [x] Build in logic to show the answer side when the card is tapped
+- [x] Push code to GitHub
+## Optional
+- [x] Toggle the flashcard between the question side and the answer side
+- [x] Style the question and answer side of the card to better distinguish between the two sides
+- [x] Add selectable multiple choice answers beneath the card
